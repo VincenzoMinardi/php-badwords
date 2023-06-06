52d9af6b70a814bd68a3515598802f764e520439
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-06-06 16:25:05 +0200

    add: input and form php

--- index.php
+++ index.php
@@ -1,12 +1,29 @@
+<?php
+
+ $text = $_POST['paragrafo'];    // Testo inserito da un utente
+
+ $textcensured = $_POST['banner'];
+
+ $textbanner = str_ireplace($banner, '***' , $text);
+
+
+
+
+
+?>
+
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
+<html>
+  <head>
+    <title>Form PHP</title>
+  </head>
+  <body>
+
+  <h1> paragrafo:<?= $text?></h1>
+  <h1><?= $textcensured?></h1>
+  <h1><?=$textbannwe?></h1>
+
+
+  </body>
 </html>
+
